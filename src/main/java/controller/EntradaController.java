ef93d0541d8e17f1f46a16ef02a3ad24f78defa0
Natallya-soaress <natallyasoares12@gmail.com>
2022-07-20 16:04:10 -0300

    correções

--- src/main/java/controller/EntradaController.java
+++ src/main/java/controller/EntradaController.java
@@ -1,19 +1,31 @@
 package controller;
 
-import com.oobj.integrador.Entrada;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
+
+
+@RequestMapping("api")
 @RestController
 public class EntradaController {
 
-    @PostMapping("api/pre-impressao")
-    public ResponseEntity<String> recebeRequisição(@RequestBody Entrada entrada){
+    @PostMapping("/pre-impressao")
+    public ResponseEntity<String> recebeRequisição(@RequestBody String entrada){
 
-        String arquivoEntrada = entrada.getEntrada();
-        System.out.println(arquivoEntrada);
+        System.out.println(entrada);
         return ResponseEntity.ok().build();
     }
-}
+
+    public void EscreveArquivo(String entrada) throws IOException {
+
+        FileWriter arquivoEntrada = new FileWriter("C:\\Users\\Nátallya-Oobj\\OneDrive\\Documentos\\entrada.txt");
+        PrintWriter gravarArquivo = new PrintWriter(arquivoEntrada);
+
+        
+
+        }
+    }
